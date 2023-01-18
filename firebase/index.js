fa1a9e9f9c82e6a8dd3dfbcbc8f1fa945ba66806
Harry <harry.pang@student.tdsb.on.ca>
2023-01-17 22:43:06 -0500

    1.0.0

---
+++ firebase/index.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCf5H1NqXJ13lDnJAhBhHzIeWK7DYsiirA",
+  authDomain: "fir-assignment-ha.firebaseapp.com",
+  projectId: "fir-assignment-ha",
+  storageBucket: "fir-assignment-ha.appspot.com",
+  messagingSenderId: "411649506945",
+  appId: "1:411649506945:web:63d4077a61d23628085780",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const firestore = getFirestore();
+
+export { auth, firestore };
